565a1fdfa814b3ebd0aadafa1e45218406deb30c
grisha <grifon.rudenko@yandex.com>
2023-08-20 21:19:41 +0300

    create useActions

--- src/recipe-item/RecipeItem.jsx
+++ src/recipe-item/RecipeItem.jsx
@@ -2,10 +2,14 @@
 import { useDispatch, useSelector } from 'react-redux';
 import css from './Recipe.module.css'
 import {actions} from '../store/favorites/favorites.slice' 
+import { useActions } from '../hooks/useActions';
 
 const RecipeItem = ({recipe}) => {
   const favorites = useSelector(state=> state.favorites);
-  const dispatch = useDispatch()
+  // нам dispatch теперь не нужен из за создания кастомного хука useActions
+  // const dispatch = useDispatch()
+  const {toggleFavorites} = useActions()
+
 console.log(favorites);
 
 // need to check condition exist
@@ -14,7 +18,7 @@ const isExist = favorites.some(r=>r.id === recipe.id)
   return (
     <div className={css.item}>
       <h2>{recipe.name}</h2>
-      <button onClick={()=>dispatch(actions.toggleFavorites(recipe))}>{
+      <button onClick={()=>toggleFavorites(recipe)}>{
         isExist ? 'Remove from' : 'Add to'
       } favorites</button>
     </div>
