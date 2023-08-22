a018412412f4bb4bd3d4ced39d095b7682a084f9
chicory <grifon.rudenko@yandex.com>
2023-08-22 21:25:30 +0300

    middleware, расширение redux-logger

--- src/store/store.js
+++ src/store/store.js
@@ -2,6 +2,12 @@ import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { reducer as favoritesReducer } from './favorites/favorites.slice'
 import { userSlice } from './users/user.slice';
 import { api } from './api/api';
+import { createLogger } from 'redux-logger';
+
+// прокидываем logger как middleware и благоря ему будем получать результаты 
+const logger = createLogger({
+	collapsed: true // позволяет получать результат более сжатого формата
+})
 
 const reducers = combineReducers({
   favorites: favoritesReducer,
@@ -13,6 +19,7 @@ export const store = configureStore({
   reducer: reducers,
 //   devTools- это инструменты разработчика для гугл расширения redux и т.д.
   devTools: true,
+//   данный middleware позволяет нам внедрить туда RTK Query. есть библиотека "redux-loger" которая выводит middleware в консоль
   middleware: (getDefaultMiddleware)=>
-  getDefaultMiddleware().concat(api.middleware)
+  getDefaultMiddleware().concat(api.middleware).concat(logger)
 })
